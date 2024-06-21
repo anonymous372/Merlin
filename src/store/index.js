b713eefbf44095aced261523987dad037045d012
Sourabh Shukla <sourabhshukla3.14@gmail.com>
2024-06-21 17:20:54 +0530

    redux store

--- src/store/index.js
+++ src/store/index.js
@@ -4,12 +4,14 @@ import {
   SET_SEARCH_QUERY,
   SET_COLORS_FILTER,
   SET_SIZE_FILTER,
+  CLEAR_ALL_FILTERS,
 } from "./actionType";
 
 const initialState = {
   search_query: "",
   colors: [],
-  size: "",
+  bird_size: -1,
+  is_filters_applied: false,
 };
 
 const filterSlice = createSlice({
@@ -18,12 +20,21 @@ const filterSlice = createSlice({
   reducers: {
     [SET_SEARCH_QUERY]: (state, action) => {
       state.search_query = action.payload;
+      state.is_filters_applied = checkAnyFiltersApplied(state);
     },
     [SET_COLORS_FILTER]: (state, action) => {
       state.colors = action.payload;
+      state.is_filters_applied = checkAnyFiltersApplied(state);
     },
     [SET_SIZE_FILTER]: (state, action) => {
-      state.size = action.payload;
+      state.bird_size = action.payload;
+      state.is_filters_applied = checkAnyFiltersApplied(state);
+    },
+    [CLEAR_ALL_FILTERS]: (state) => {
+      state.search_query = "";
+      state.colors = [];
+      state.bird_size = -1;
+      state.is_filters_applied = false;
     },
   },
 });
@@ -31,6 +42,14 @@ const filterSlice = createSlice({
 const store = configureStore({
   reducer: filterSlice.reducer,
 });
+
+// Helper Function
+const checkAnyFiltersApplied = (state) => {
+  if (state.search_query) return true;
+  if (state.colors.length) return true;
+  if (state.bird_size >= 0) return true;
+  return false;
+};
 
 export const actions = filterSlice.actions;
 export default store;
